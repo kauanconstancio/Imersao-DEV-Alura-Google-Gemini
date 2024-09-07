cc21636c32ee04725b86c9547760302674c78e83
Kauan Constancio <146592614+kauanconstancio@users.noreply.github.com>
2024-09-07 12:09:35 -0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,42 @@
+function pesquisar() {
+    // Obtém o elemento HTML onde os resultados serão exibidos.
+    let pesquisa = document.getElementById("pesquisa");
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    if(campoPesquisa == "") {
+        pesquisa.innerHTML = "<h5>Nenhum jogo foi encontrado :(</h5>"
+        return
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+    // Inicializa uma string vazia para armazenar os resultados da pesquisa.
+    let resultado = "";
+    let nome = "";
+    let genero = "";
+    let descricao = "";
+
+    for (let dado of dados) {
+        nome = dado.nome.toLowerCase()
+        genero = dado.genero.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        if (nome.includes(campoPesquisa) || genero.includes(campoPesquisa) || descricao.includes(campoPesquisa)) {
+            resultado += `
+                <div class="resultado">
+                    <img src="${dado.banner}">
+                    <div class="conteudo">
+                        <h2>${dado.nome}</h2>
+                        <p>${dado.descricao}</p>
+                        <p>${dado.lancamento}</p>
+                        <p>${dado.genero}</p>
+                        <p>${dado.estudios}</p>
+                        <a href="${dado.link}" target="_blank">Baixe Aqui</a>
+                    </div>
+                </div>
+            `;
+        }
+    }
+
+    // Atualiza o conteúdo do elemento "pesquisa" com os resultados.
+    pesquisa.innerHTML = resultado;
+}
